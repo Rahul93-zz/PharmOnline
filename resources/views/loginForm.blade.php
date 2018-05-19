7069d32e616eed95ac5dee48da56d458f757a5b5
rahulkathuria <kathuriarahul93@gmail.com>
2018-05-19 19:27:02 +0530

    changes

---
+++ resources/views/loginForm.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+<div>
+    <span>Login</span>
+    <span>Signup</span>
+    <div id="login_container">
+        <input type="text" id="username">
+        <input type="password" id="password">
+    </div>
+    <div id="signup_container">
+        <input type="text" id="signup_name">
+        <input type="text" id="signup_email">
+        <input type="number" id="signup_mobile">
+        <input type="password" id="signup_password">
+    </div>
+</div>
