06217fbb572400fd30e61c7bf778899a37c34a9a
rahulkathuria <kathuriarahul93@gmail.com>
2018-05-19 19:30:22 +0530

    changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,12 +3,11 @@
     <title>App Name - @yield('title')</title>
 </head>
 <body>
-@section('sidebar')
-This is the master sidebar.
-@show
-
 <div class="container">
     @yield('content')
 </div>
 </body>
+<link defer href="{{ asset('css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
+<script defer type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </html>
--- resources/views/loginForm.blade.php
+++ resources/views/loginForm.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.master')
 <div>
-    <span>Login</span>
-    <span>Signup</span>
-    <div id="login_container">
+    <span class="user_toggle" data-type="login">Login</span>
+    <span class="user_toggle" data-type="signup">Signup</span>
+    <div class="user_container" data-container="login" id="login_container">
+        <label for="username">Username</label>
         <input type="text" id="username">
+        <label for="password">Password</label>
         <input type="password" id="password">
+        <span id="login_submit">Login</span>
     </div>
-    <div id="signup_container">
+    <div class="user_container hidden" data-container="signup" id="signup_container">
+        <label for="signup_name">Name</label>
         <input type="text" id="signup_name">
+        <label for="signup_email">Email</label>
         <input type="text" id="signup_email">
+        <label for="signup_mobile">Mobile</label>
         <input type="number" id="signup_mobile">
+        <label for="signup_password">Password</label>
         <input type="password" id="signup_password">
+        <span id="signup_submit">Signup</span>
     </div>
 </div>
